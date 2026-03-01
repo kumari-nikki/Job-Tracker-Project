53580b16cc6bc603387e0ef8e84bdf1b33d0b56c
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-02 02:04:05 +0530

    Add user routes: register, login, and profile update with authentication middleware

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-
+import userRoute from "./routes/user.route.js";
 dotenv.config({})
 //middleware
 const app= express();
@@ -17,6 +17,8 @@ app.use(cors({
 }))
 // mongoose cnnection
 const PORT=process.env.PORT||3000;
+//api's
+app.use("/api/v1/user",userRoute);
 app.listen(PORT,()=>{
     connectDB();
     console.log("server is running at port 3000")
