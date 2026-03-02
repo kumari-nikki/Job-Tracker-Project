fe8733e08dd9334e629e05e765e7863cac47cace
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-02 18:20:44 +0530

    Fix update profile logic and improve validation handling

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -117,14 +117,14 @@ export const updateProfile = async (req, res) => {
     try {
         const { fullName, email, phoneNumber, bio, skills } = req.body;
         const file = req.file;
-        if (!fullName || !email || !phoneNumber || !bio || !skills) {
-            return res.status(400).json({
-                message: "something is missing",
-                success: false
-            });
-        };
+       
         //cloudinary comes here
-        const skillsArray = skills.split(",");
+        let skillsArray ;
+        if(skills)
+        {
+           skillsArray = skills.split(",");   
+        }
+        
         const userId = req.id;//middleware authentication
         let user = await User.findById(userId);
         if (!user) {
@@ -134,11 +134,11 @@ export const updateProfile = async (req, res) => {
             });
         }
         //updating the data
-        user.fullName = fullName,
-            user.email = email,
-            user.phoneNumber = phoneNumber,
-            user.profile.bio = bio,
-            user.profile.skills = skillsArray
+        if(fullName)  user.fullName = fullName
+           if(email) user.email = email
+            if(phoneNumber) user.phoneNumber = phoneNumber
+           if(bio) user.profile.bio = bio
+            if(skills) user.profile.skills = skillsArray
         //resume comes later here
         await user.save();
         user = {
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,14 @@
 
 import express from "express";
-import { register,login, updateProfile } from "../controllers/user.controller.js";
+import { register,login, updateProfile, logout } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router=express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/profile/update").post(isAuthenticated,updateProfile);
+router.route("/logout").post(logout);
+
 export default router;
 
 
