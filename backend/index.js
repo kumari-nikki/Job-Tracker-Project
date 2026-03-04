e651a0af02388194167e840a26bdc7ce4d910e78
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-04 13:48:45 +0530

    feat: add job application controller and routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js"
 dotenv.config({})
 //middleware
 const app= express();
@@ -23,6 +24,8 @@ const PORT=process.env.PORT||3000;
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
+
 app.listen(PORT,()=>{
     connectDB();
     console.log("server is running at port 3000")
