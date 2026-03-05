b2e75b7c219f1c20f9f5f473a097097f53881839
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-05 20:00:29 +0530

    refactor: integrate Redux Toolkit for state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Toaster } from "sonner";
+import { Toaster } from "sonner"
+import { Provider } from "react-redux"
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-     <Toaster />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <Toaster />
   </StrictMode>,
 )
