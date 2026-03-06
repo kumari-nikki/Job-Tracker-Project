0d614f88101f30fdf9516a51f8fd608a14d90a1b
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-06 14:51:26 +0530

    add Herosection page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,13 @@
 
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './Herosection'
 function Home() {
   return (
     <div>
       <Navbar/>
+      <HeroSection/>
+    
     </div>
   )
 }
