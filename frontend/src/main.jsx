8f0aa1f4e68990407ecb4ccb5cdbbdf1cf4260f0
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-12 17:06:44 +0530

    feat: integrate redux-persist to persist redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from "sonner"
 import { Provider } from "react-redux"
-import { store } from './redux/store';
+import { store, persistor } from './redux/store.js'; // ✅ import persistor directly
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
     <Toaster />
   </StrictMode>,
