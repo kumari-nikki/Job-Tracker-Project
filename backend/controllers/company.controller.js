141591023f3217cd795c8ce220648d6a97cebe05
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-18 19:10:04 +0530

    feat(company): add Cloudinary integration in company controller for logo upload

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -1,4 +1,6 @@
 import {Company} from "../models/company.model.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 // Register Company
 export const registerCompany = async (req, res) => {
@@ -107,8 +109,13 @@ export const getCompanyById = async (req, res) => {
 export const updateCompany = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
+  const file=req.file;
+  // cloudinary comes here
+  const fileUri= getDataUri(file);
+  const cloudResponse= await cloudinary.uploader.upload(fileUri.content);
+  const logo= cloudResponse.secure_url;
 
-        const updateData = { name, description, website, location };
+        const updateData = { name, description, website, location, logo};
 
         const company = await Company.findByIdAndUpdate(
             req.params.id,
