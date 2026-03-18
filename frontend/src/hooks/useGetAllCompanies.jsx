7d0bf1533f5537da1c78daf204d1e1a5e583d082
Kumari Nikki <nikkibellla66@gmail.com>
2026-03-18 20:14:40 +0530

    feat: render company names and logos dynamically in CompaniesTable

--- frontend/src/hooks/useGetAllCompanies.jsx
+++ frontend/src/hooks/useGetAllCompanies.jsx
@@ -8,7 +8,7 @@ const useGetAllCompanies = () => {
     useEffect(() => {
         const fetchCompanies = async () => {
             try {
-                const res = await axios.get(`${COMPANY_API_END_POINT}/get/`, { withCredentials: true });
+                const res = await axios.get(`${COMPANY_API_END_POINT}/get`, { withCredentials: true });
                 if (res.data.success) {
                     dispatch(setCompanies(res.data.companies))
                 }
@@ -18,7 +18,7 @@ const useGetAllCompanies = () => {
             }
         }
       fetchCompanies();
-    }, [])
+    }, [dispatch])
 }
 
 export default useGetAllCompanies;
